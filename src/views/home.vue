b2efcaa2f016dd1cbee3b0b60892ca3c0abe585f
YeZhengXiong <382611619@qq.com>
2023-09-07 11:44:08 +0800

    feat: 增加pinia

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,27 +1,42 @@
-<script setup lang="ts"></script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div>姓名：{{ userInfo.name }} 年龄：{{ userInfo.age }}</div>
+    <div>token：{{ token }}</div>
+    <div>getter值：{{ newName }}</div>
+    <button @click="handleUser">更新用户</button>
+    <button @click="handleAge">更新年龄</button>
+    <button @click="handleToken">更新token</button>
+    <button @click="goLocation">跳转招行</button>
   </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+import { useUserStore } from '@/store/module/user'; //路径别名，引入store
+
+const goLocation = () => {
+  window.location.href =
+    'https://jytfz.paas.cmbchina.com/pfc-member/merchant/base/register/a2V5PUpZVDAwMDUxX0NfOTEzMjAzMTE3NjUxMzE0OTFDO3NlcT1CQzgyQzQzMEMzRDkwNzhEQkIwNzFBMTMyRjUzRUI5Rg==';
+};
+
+const userStore = useUserStore();
+
+//storeToRefs 会跳过所有的 action 属性
+const { userInfo, token, newName } = storeToRefs(userStore);
+
+//action 属性直接解构
+const { updateUserInfo, updateAge, updateToken } = userStore;
+
+const handleUser = () => {
+  updateUserInfo({ name: 'lisi', age: 24 });
+};
+
+const handleAge = () => {
+  //userInfo是一个ref响应式引用，需通过.value取值
+  updateAge(userInfo.value.age + 1);
+};
+
+const handleToken = () => {
+  updateToken('23234');
+};
+</script>
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,29 +1,43 @@
-<script setup lang="ts">
-import VueSvg from '@/assets/vue.svg';
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img :src="VueSvg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div>姓名：{{ userInfo.name }} 年龄：{{ userInfo.age }}</div>
+    <div>token：{{ token }}</div>
+    <div>getter值：{{ newName }}</div>
+    <button @click="handleUser">更新用户</button>
+    <button @click="handleAge">更新年龄</button>
+    <button @click="handleToken">更新token</button>
+    <button @click="goLocation">跳转招行</button>
+    <!--    <iframe src="http://localhost:5173/home" width="600" height="400"></iframe>-->
   </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+import { useUserStore } from '@/store/module/user'; //路径别名，引入store
+
+const goLocation = () => {
+  location.href =
+    'https://jytfz.paas.cmbchina.com/pfc-member/merchant/base/register/a2V5PUpZVDAwMDUxX0NfOTEzMjAzMTE3NjUxMzE0OTFDO3NlcT1CQzgyQzQzMEMzRDkwNzhEQkIwNzFBMTMyRjUzRUI5Rg==';
+};
+
+const userStore = useUserStore();
+
+//storeToRefs 会跳过所有的 action 属性
+const { userInfo, token, newName } = storeToRefs(userStore);
+
+//action 属性直接解构
+const { updateUserInfo, updateAge, updateToken } = userStore;
+
+const handleUser = () => {
+  updateUserInfo({ name: 'lisi', age: 24 });
+};
+
+const handleAge = () => {
+  //userInfo是一个ref响应式引用，需通过.value取值
+  updateAge(userInfo.value.age + 1);
+};
+
+const handleToken = () => {
+  updateToken('23234');
+};
+</script>
