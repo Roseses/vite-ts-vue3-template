1a14c959f2173b76cf2ae0b8f0063bde47bf99d8
YeZhengXiong <382611619@qq.com>
2023-09-07 14:26:45 +0800

    feat: 增加element-plus

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,12 @@ import './style.css';
 import App from './App.vue';
 import router from './router/index';
 import store from './store';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'; //引入图标
+import 'element-plus/dist/index.css'; //引入样式
 
-createApp(App).use(router).use(store).mount('#app');
+const app = createApp(App);
+//引入图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
+app.use(router).use(store).mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path'; //这个path用到了上面安装的@types/node
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +14,17 @@ export default defineConfig({
       },
     },
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // 按需引入element-plus
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    // 按需引入element-plus
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   //这里进行配置别名
   resolve: {
     alias: {
