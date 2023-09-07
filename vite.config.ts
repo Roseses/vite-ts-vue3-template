c82471fe9618d8e119740f0b42c77dbb219155a3
YeZhengXiong <382611619@qq.com>
2023-09-07 22:00:50 +0800

    feat: 增加打包可视化分析

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'; //这个path用到了上面安装的@types/node
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -39,6 +40,13 @@ export default defineConfig({
   },
   plugins: [
     vue(),
+    // 包大小可视化分析
+    visualizer({
+      open: true, //在默认用户代理中打开生成的文件
+      gzipSize: true, // 收集 gzip 大小并将其显示
+      brotliSize: true, // 收集 brotli 大小并将其显示
+      filename: 'stats.html', // 分析图生成的文件名
+    }),
     // 按需引入element-plus
     AutoImport({
       resolvers: [ElementPlusResolver()],
